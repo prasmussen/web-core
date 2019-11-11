1e6d5c85e52f3e54b005368f42b4dd390fa88b1c
Petter Rasmussen <petter@hask.no>
2019-11-11 13:14:09 +0100

    Add Read module

---
+++ src/WebToolkit/Read.hs
@@ -0,0 +1,52 @@
+module WebToolkit.Read
+    ( read
+    , readString
+    , readText
+    , readBool
+    ) where
+
+
+import Prelude hiding (read)
+
+import qualified Data.Text as T
+import qualified Text.Read as Read
+
+
+
+read :: Read b => (b -> a) -> String -> [(a, String)]
+read constructor str =
+    case Read.readMaybe str of
+        Just value ->
+            [(constructor value, "")]
+
+        Nothing ->
+            []
+
+
+readString :: (String -> a) -> String -> [(a, String)]
+readString fromString str =
+    [(fromString str, "")]
+
+
+readText :: (T.Text -> a) -> String -> [(a, String)]
+readText constructor str =
+    [(constructor $ T.pack str, "")]
+
+
+readBool :: (Bool -> a) -> String -> [(a, String)]
+readBool constructor str =
+    case str of
+        "true" ->
+            [(constructor True, "")]
+
+        "false" ->
+            [(constructor False, "")]
+
+        "1" ->
+            [(constructor True, "")]
+
+        "0" ->
+            [(constructor False, "")]
+
+        _ ->
+            []
