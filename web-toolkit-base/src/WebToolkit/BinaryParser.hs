9b07949176013ea3fd6f40325319757ac4e57cd7
Petter Rasmussen <petter@hask.no>
2019-11-11 18:54:36 +0100

    Add BinaryParser module

---
+++ web-toolkit-base/src/WebToolkit/BinaryParser.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module WebToolkit.BinaryParser
+    ( parser
+    , maybeParser
+    ) where
+
+
+import BinaryParser (BinaryParser)
+
+import qualified BinaryParser
+import qualified Data.ByteString as BS
+import qualified Data.Text.Encoding as TE
+
+
+
+parser :: (BS.ByteString -> a) -> BinaryParser a
+parser mapper =
+    mapper <$> BinaryParser.remainders
+
+
+maybeParser :: (BS.ByteString -> Maybe a) -> BinaryParser a
+maybeParser mapper = do
+    remainders <- BinaryParser.remainders
+    case mapper remainders of
+        Just res ->
+            return res
+
+        Nothing ->
+            BinaryParser.failure $ mconcat
+                [ "Failed to parse: «"
+                , TE.decodeUtf8 remainders
+                , "»"
+                ]
