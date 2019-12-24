d3b748d3919847aec596b26b714b7372486a0a7e
Petter Rasmussen <petter@hask.no>
2019-12-24 15:36:10 +0100

    Replace mailgun lib with http request

--- web-core-base/src/WebCore/Mailgun.hs
+++ web-core-base/src/WebCore/Mailgun.hs
@@ -1,25 +1,32 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module WebCore.Mailgun
     ( Config(..)
     , Message(..)
-    , Envelope
-    , envelope
+    , Error(..)
+    , prepareRequest
     , send
+    , formatMessage
     ) where
 
 
 import Data.Function ((&))
 
+import qualified Control.Exception as Exception
+import qualified Data.Aeson as Aeson
 import qualified Data.Bifoldable as Bifoldable
-import qualified Data.Bifunctor as Bifunctor
-import qualified Data.Coerce as Coerce
+import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
-import qualified Mail.Hailgun as Hailgun
+import qualified GHC.Generics as GHC
+import qualified Network.HTTP.Client as Client
+import qualified Network.HTTP.Client.TLS as TLS
 import qualified WebCore.Read as Read
 
+
+-- CONFIG
 
 data Config = Config
     { domain :: Domain
@@ -27,6 +34,17 @@ data Config = Config
     , sender :: Sender
     }
     deriving (Show)
+
+
+domainText :: Config -> T.Text
+domainText Config{domain = Domain domain} =
+    domain
+
+
+apiKeyBS :: Config -> BS.ByteString
+apiKeyBS Config{apiKey = ApiKey apiKey} =
+    TE.encodeUtf8 apiKey
+
 
 
 -- DOMAIN
@@ -72,87 +90,66 @@ instance Read Sender where
 
 
 data Message = Message
-    { subject :: T.Text
-    , content :: T.Text
-    , recipient :: T.Text
+    { from :: T.Text
+    , to :: T.Text
+    , subject :: T.Text
+    , text :: T.Text
     }
+    deriving (Show, GHC.Generic)
+
+
+instance Aeson.ToJSON Message
+
+
+formatMessage :: Message -> T.Text
+formatMessage Message{..} =
+    [ ("FROM", from)
+    , ("TO", to)
+    , ("SUBJECT", subject)
+    , ("CONTENT", text)
+    ]
+    & map Bifoldable.biList
+    & map (T.intercalate ": ")
+    & T.intercalate "\n"
+
+
+
+-- REQUEST
+
+
+prepareRequest :: Config -> Message -> Client.Request
+prepareRequest config Message{..} =
+    mconcat
+        [ "https://api.eu.mailgun.net/v3/"
+        , domainText config
+        , "/messages"
+        ]
+        & T.unpack
+        & Client.parseRequest_
+        & Client.setRequestCheckStatus
+        & Client.applyBasicAuth "api" (apiKeyBS config)
+        & Client.urlEncodedBody
+            [ ("from", TE.encodeUtf8 from)
+            , ("to", TE.encodeUtf8 to)
+            , ("subject", TE.encodeUtf8 subject)
+            , ("text", TE.encodeUtf8 text)
+            ]
+
+
+
+data Error
+    = RequestException Client.HttpException
     deriving (Show)
 
 
+send :: Client.Request -> IO (Either Error ())
+send request = do
+    manager <- TLS.newTlsManager
+    result <- Client.httpLbs request manager
+        & Exception.try
+    case result of
+        Left err ->
+            pure $ Left (RequestException err)
 
-messageToHailgunMessage :: Config -> Message -> Either String Hailgun.HailgunMessage
-messageToHailgunMessage Config{sender = Sender sender} Message{..} =
-    Hailgun.hailgunMessage
-        subject
-        (textContent content)
-        (TE.encodeUtf8 sender)
-        (singleRecipient recipient) []
-
-
--- ENVELOPE
-
-
-data Envelope = Envelope
-    { message :: Hailgun.HailgunMessage
-    , context :: Hailgun.HailgunContext
-    , formatted :: T.Text
-    }
-
-
-instance Show Envelope where
-    show Envelope{..} =
-        T.unpack formatted
-
-
-
-envelope :: Config -> Message -> Either T.Text Envelope
-envelope config@Config{..} message@Message{..} = do
-    msg <- messageToHailgunMessage config message
-        & Bifunctor.first T.pack
-    pure $ Envelope
-        { message = msg
-        , context =
-            Hailgun.HailgunContext
-                { hailgunDomain = T.unpack (Coerce.coerce domain)
-                , hailgunApiKey = T.unpack (Coerce.coerce apiKey)
-                , hailgunProxy = Nothing
-                }
-        , formatted =
-            [ ("FROM", Coerce.coerce sender)
-            , ("TO", recipient)
-            , ("SUBJECT", subject)
-            , ("CONTENT", content)
-            ]
-            & map Bifoldable.biList
-            & map (T.intercalate ": ")
-            & T.intercalate "\n"
-        }
-
-
-
-send :: Envelope -> IO (Either T.Text ())
-send Envelope{..} = do
-    res <- Hailgun.sendEmail context message
-    pure $ res
-        & Bifunctor.first Hailgun.herMessage
-        & Bifunctor.first T.pack
-        & Bifunctor.second (pure ())
-
-
-
--- HELPER FUNCTIONS
-
-
-textContent :: T.Text -> Hailgun.MessageContent
-textContent content =
-    Hailgun.TextOnly (TE.encodeUtf8 content)
-
-
-
-singleRecipient :: T.Text -> Hailgun.MessageRecipients
-singleRecipient recipient =
-    Hailgun.MessageRecipients
-        { recipientsTo = [TE.encodeUtf8 recipient]
-        , recipientsCC = []
-        , recipientsBCC = []
-        }
+        Right _ ->
+            pure $ Right ()
